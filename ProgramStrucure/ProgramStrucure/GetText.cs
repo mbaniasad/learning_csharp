ab32570ef85468ca9f793f5e4f84af41af5fb345
unknown <mahsa.poorjafari@gmail.com>
2019-06-09 10:18:38 +0200

    print input of user

---
+++ ProgramStrucure/ProgramStrucure/GetText.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ProgramStructure
+{
+    class GetText
+    {
+        public void PrintText()
+        {
+            String input = "";
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Please enter your Text");
+                input = Console.ReadLine();
+                if (String.IsNullOrEmpty(input))
+                {
+                    input = "Input is Empty";
+                    Console.WriteLine(input);
+                    break;
+                }
+                Console.WriteLine(input);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ProgramStrucure/ProgramStrucure/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgramStructure
+{
+    class Program
+    {
+        /// <summary>
+        /// static
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.ReadLine();
+            GetText firstObj = new GetText();
+            firstObj.PrintText();
+        }
+    }
+
+}
